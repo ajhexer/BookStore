ec12444e447d0d1fa1a7e70cbe3e3837f5bf6ac2
Ajhexer <alirezajelokhani559@yahoo.com>
2021-08-15 18:40:08 +0430

    Added review model

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Review
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'price')
-
+    inlines = [ReviewInline]
 
 admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 import uuid
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -15,3 +16,9 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
