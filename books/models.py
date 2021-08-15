bede742eca313ba73b5ad652b8f5beb7b26380cf
Ajhexer <alirezajelokhani559@yahoo.com>
2021-08-15 06:53:14 +0430

    Added file upload feature

--- books/models.py
+++ books/models.py
@@ -8,6 +8,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(decimal_places=2, max_digits=6)
+    cover = models.ImageField(upload_to='covers/')
 
     def __str__(self):
         return self.title
